f03c92664708a810e058b16ed8940a04dae9a5e3
sanjadrinic <sanja.drinic@uns.ac.rs>
2022-12-10 20:41:21 +0100

    feat: logging bank microservice

---
+++ microservices/bank/src/main/java/com/project/bank1/service/BankService.java
@@ -0,0 +1,27 @@
+package com.project.bank1.service;
+
+import com.project.bank1.dto.OnboardingRequestDto;
+import com.project.bank1.dto.RequestDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+public class BankService {
+
+    public RequestDto validateAcquirer(OnboardingRequestDto dto, String bankFrontendUrl) {
+        RequestDto request = new RequestDto();
+        request.setAmount(dto.getAmount());
+        request.setMerchantId(dto.getMerchantId());
+        // TODO SD: na osnovu merchant id -> izvuci merchant pass
+        request.setMerchantPassword("iwoznljjrgwfinrkylmwzaqpsijhutdailaylwhxfoucnoncxapigphrucedmolsicyurvntbnvmhsrcpzleilsjlkocwiitvyde");
+        request.setMerchantOrderId(dto.getMerchantOrderId());
+        request.setMerchantTimestamp(LocalDateTime.now());
+        request.setSuccessUrl(bankFrontendUrl + "/success");
+        request.setFailedUrl(bankFrontendUrl + "/failed");
+        request.setErrorUrl(bankFrontendUrl + "/error");
+        return request;
+    }
+
+}
