9e4db23bb07c55ba8f9017ead5ac27c209bdc871
sanjadrinic <sanja.drinic@uns.ac.rs>
2023-01-08 15:11:59 +0100

    feat: logging in credit card service in PSP

--- microservices/bank/src/main/java/com/project/bank1/controller/BankController.java
+++ microservices/bank/src/main/java/com/project/bank1/controller/BankController.java
@@ -1,5 +1,6 @@
 package com.project.bank1.controller;
 
+import com.project.bank1.service.LoggerService;
 import com.project.bank1.service.interfaces.BankService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -14,9 +15,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class BankController {
     @Autowired
     private BankService bankService;
+    private LoggerService loggerService = new LoggerService(this.getClass());
+
 
     @RequestMapping(method = RequestMethod.GET, value = "/getAll")
     public ResponseEntity<?> getAll() {
+        loggerService.infoLog("Getting all banks from bank microservice");
         return new ResponseEntity<>(bankService.getAll(), HttpStatus.OK);
     }
 }
