d2f6d992bf55e15c0ce0ff9f01fc7cc326a81ae0
kukuckarastislav <73312345+kukuckarastislav@users.noreply.github.com>
2023-01-29 18:24:45 +0100

    feat: confirm and cancel payment
    succes and cancel url

--- microservices/crypto/src/main/java/com/project/crypto/service/PaymentService.java
+++ microservices/crypto/src/main/java/com/project/crypto/service/PaymentService.java
@@ -9,6 +9,7 @@ import com.project.crypto.model.Merchant;
 import com.project.crypto.repository.CryptoOrderRepository;
 import com.project.crypto.repository.MerchantRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -27,25 +28,28 @@ public class PaymentService {
     @Autowired
     private CryptoOrderRepository cryptoOrderRepository;
 
-    String urlCoinGate = "https://api-sandbox.coingate.com/api/v2";
+    @Value("${coingate.api}")
+    String urlCoinGate;// = "https://api-sandbox.coingate.com/api/v2";
+
+    @Value("${pspfront}")
+    String urlPSPFront; // = "http://localhost:4200";
 
     public CreateOrderResponse createOrder(CreateOrderDto createOrderDto) {
-
-        String urlPSPCrypto = "http://localhost:8000/crypto-service/";
+        System.out.println("coingate URL:" + urlCoinGate);
         //URLEncoder.encode("", StandardCharsets.UTF_8);
 
         Merchant merchant = merchantRepository.findByMerchantId(createOrderDto.getShopId());
 
         StringBuilder body = new StringBuilder();
         body.append("order_id=").append(createOrderDto.getTransactionId()).append("&");
-        body.append("price_amount=").append("0.1").append("&"); //createOrderDto.getAmount()
+        body.append("price_amount=").append(createOrderDto.getAmount()).append("&"); //createOrderDto.getAmount()
         body.append("price_currency=EUR").append("&");
         body.append("receive_currency=EUR").append("&");
         body.append("title=").append(createOrderDto.getShopId()+"_"+createOrderDto.getTransactionId()).append("&");
 
-        body.append("callback_url=").append(URLEncoder.encode(urlPSPCrypto+"callback", StandardCharsets.UTF_8)).append("&");
-        body.append("cancel_url=").append(URLEncoder.encode(urlPSPCrypto+"cancel-order/", StandardCharsets.UTF_8)).append(createOrderDto.getTransactionId()).append("&");
-        body.append("success_url=").append(URLEncoder.encode(urlPSPCrypto+"confirm-order/", StandardCharsets.UTF_8)).append(createOrderDto.getTransactionId());
+        //body.append("callback_url=").append(URLEncoder.encode(urlPSPCrypto+"callback", StandardCharsets.UTF_8)).append("&");
+        body.append("cancel_url=").append(URLEncoder.encode(urlPSPFront+"/crypto-cancel/", StandardCharsets.UTF_8)).append(createOrderDto.getTransactionId()).append("&");
+        body.append("success_url=").append(URLEncoder.encode(urlPSPFront+"/crypto-success/", StandardCharsets.UTF_8)).append(createOrderDto.getTransactionId());
 
 
         HttpRequest request = HttpRequest.newBuilder()
@@ -77,24 +81,39 @@ public class PaymentService {
 
     }
 
+    @Value("${webshop.url.cancel}")
+    String merchantCancelCallbackLink;
     public CancelPaymentResponseDto cancelOrder(String orderId) {
-
+        updateOrderStatus(orderId);
         CryptoOrder cryptoOrder = cryptoOrderRepository.findByOrderId(orderId);
-        cryptoOrder.setStatus(CryptoOrderStatus.CANCELED);
-        cryptoOrderRepository.saveAndFlush(cryptoOrder);
-        String merchantCallbackLink = "http://localhost:9000/api/orders/cancel/" + orderId;
 
-        return new CancelPaymentResponseDto(true, merchantCallbackLink);
+        String merchantCallbackLink = merchantCancelCallbackLink + orderId; //"http://localhost:4201/cancel/"
+
+        if(cryptoOrder.getStatus() == CryptoOrderStatus.CANCELED){
+            //TODO ? da obavestimo webshop bekend?
+            return new CancelPaymentResponseDto(true, merchantCallbackLink);
+        }else{
+            return new CancelPaymentResponseDto(false, "");
+        }
     }
 
+    @Value("${webshop.url.success}")
+    String merchantSuccessCallbackLink;
     public ConfirmPaymentResponseDto confirmOrder(String orderId) {
-
+        updateOrderStatus(orderId);
         CryptoOrder cryptoOrder = cryptoOrderRepository.findByOrderId(orderId);
-        cryptoOrder.setStatus(CryptoOrderStatus.PAID);
-        cryptoOrderRepository.saveAndFlush(cryptoOrder);
-        String merchantCallbackLink = "http://localhost:9000/api/orders/confirm/" + orderId;
 
-        return new ConfirmPaymentResponseDto(true, merchantCallbackLink);
+        String merchantCallbackLink = merchantSuccessCallbackLink + orderId; // "http://localhost:4201/success/"
+
+        if(cryptoOrder.getStatus() == CryptoOrderStatus.PAID){
+            return new ConfirmPaymentResponseDto(true, merchantCallbackLink);
+        }else{
+            return new ConfirmPaymentResponseDto(false, "");
+        }
+    }
+
+    private boolean updateOrderStatus(String orderId){
+        return getOrderStatus(orderId) != null;
     }
 
     public OrderStatusDto getOrderStatus(String orderId) {
